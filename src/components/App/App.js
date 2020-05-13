1fd82934b33aa75a617fe8323bd44a580f038e0d
Brian Roxbury <roxburybrian@gmail.com>
2020-05-13 17:42:30 -0600

    Add fetch functionality to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Login from '../LogIn/Login';
 import { Route, Redirect } from 'react-router-dom';
 import Header from '../Header/Header.js';
 import AreaContainer from '../AreaContainer/AreaContainer.js';
+// import DataManager from '../../DataManager/DataManager.js'
 
 class App extends Component {
   constructor(props) {
@@ -19,6 +20,30 @@ class App extends Component {
       areas: [],
     };
   }
+
+  componentDidMount() {
+    const url = 'https://vrad-api.herokuapp.com'
+    fetch(url + '/api/v1/areas')
+     .then(response => response.json())
+     .then(areaData => {
+       const areaPromises = areaData.areas.map(area => {
+         return fetch(url + area.details)
+         .then(response => response.json())
+         .then(info => {
+           return {
+             id: info.id,
+             name: info.name,
+             location: info.location,
+             about: info.about,
+             listings: info.listings
+           }
+         })
+       })
+       Promise.all(areaPromises).then(completeAreaData => this.setState({ areas: completeAreaData }))
+     })
+  }
+    
+  
 
   setLoginInfo = (info) => {
     const { username, email, accountType } = info;
